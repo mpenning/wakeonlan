770acf6c2a1f7033ca86b03198540c12373c9d8c
mpenning <mike@pennington.net>
2014-04-19 17:23:23 -0500

    Update README

--- wakeonlan/wol.py
+++ wakeonlan/wol.py
@@ -31,7 +31,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 """
 
 ## Docstring props: http://stackoverflow.com/a/1523456/667301
-__version_tuple__ = (0,0,1)
+__version_tuple__ = (0,0,2)
 __version__ = '.'.join(map(str, __version_tuple__))
 __email__ = "mike /at\ pennington [dot] net"
 __author__ = "David Michael Pennington <{0}>".format(__email__)
